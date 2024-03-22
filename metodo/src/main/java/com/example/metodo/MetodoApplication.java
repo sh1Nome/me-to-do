dad57d1940030f103ca997b880d0b359886ea5ee
sh1Nome <shinodayo1024@gmail.com>
2024-03-22 12:30:25 +0000

    [add]仮

---
+++ metodo/src/main/java/com/example/metodo/MetodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.metodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodoApplication.class, args);
+	}
+
+}
---
+++ metodo/src/test/java/com/example/metodo/MetodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
