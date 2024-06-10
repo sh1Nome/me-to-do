1d66ae9a85899536d95080eb320e86f940db19b7
sh1Nome <shinodayo1024@gmail.com>
2024-06-10 22:46:37 +0900

    update ログインとログアウト(インメモリ)

---
+++ work/metodo/src/main/java/com/re_kid/metodo/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.re_kid.metodo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(login -> login
+            .loginPage("/login")
+            .permitAll()
+        )
+        .authorizeHttpRequests(authz -> authz
+            .anyRequest()
+            .authenticated()
+        )
+        .logout(logout -> logout
+            .permitAll()
+        );
+        return http.build();
+    }
+
+}
--- work/metodo/src/main/java/com/re_kid/metodo/controller/LoginController.java
+++ work/metodo/src/main/java/com/re_kid/metodo/controller/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TopController {
+public class LoginController {
 
-    @GetMapping({"/", "top"})
-    public String getTop() {
-        return "top";
+    @GetMapping("login")
+    public String getLogin() {
+        return "login";
     }
 
 }
