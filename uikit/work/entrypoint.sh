534832e1c27aec7b4166a7ac00983443e055158e
sh1Nome <shinodayo1024@gmail.com>
2024-07-13 10:25:59 +0900

    update UIkitバージョン変数管理

--- uikit/work/entrypoint.sh
+++ uikit/work/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cp /work/customized-uikit.less /uikit/uikit-3.21.6/src/less/
-cd /uikit/uikit-3.21.6/src/less
+cp /work/customized-uikit.less /uikit/uikit-${UIKIT_VERSION}/src/less/
+cd /uikit/uikit-${UIKIT_VERSION}/src/less
 lessc customized-uikit.less customized-uikit.css
-mv /uikit/uikit-3.21.6/src/less/customized-uikit.css /work/
-mv /uikit/uikit-3.21.6/dist/js/uikit.min.js /work/
-mv /uikit/uikit-3.21.6/dist/js/uikit-icons.min.js /work/
+mv /uikit/uikit-${UIKIT_VERSION}/src/less/customized-uikit.css /work/
+mv /uikit/uikit-${UIKIT_VERSION}/dist/js/uikit.min.js /work/
+mv /uikit/uikit-${UIKIT_VERSION}/dist/js/uikit-icons.min.js /work/
