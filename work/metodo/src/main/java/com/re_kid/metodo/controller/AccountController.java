873939790fca4684ffd91ee4572f074513e2d0ab
sh1Nome <shinodayo1024@gmail.com>
2024-07-13 21:55:45 +0900

    update コントローラー

--- work/metodo/src/main/java/com/re_kid/metodo/controller/AccountController.java
+++ work/metodo/src/main/java/com/re_kid/metodo/controller/AccountController.java
@@ -2,13 +2,19 @@ package com.re_kid.metodo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
 
     @GetMapping("account_new")
-    public String getAccountEdit() {
+    public String getAccountNew() {
         return "account_new";
+    }
+
+    @PostMapping("account_new")
+    public String postAccountNew() {
+        return "redirect:/";
     }
 
 }
