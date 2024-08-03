5e147b41dbf1d824202eaabfb64ecde343893e0b
sh1Nome <shinodayo1024@gmail.com>
2024-08-03 23:54:30 +0900

    update メッセージ

--- work/metodo/src/main/java/com/re_kid/metodo/controller/AccountController.java
+++ work/metodo/src/main/java/com/re_kid/metodo/controller/AccountController.java
@@ -1,12 +1,16 @@
 package com.re_kid.metodo.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.re_kid.metodo.form.AccountForm;
 import com.re_kid.metodo.service.AccountService;
@@ -16,8 +20,11 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    public AccountController(AccountService accountService) {
+    private final MessageSource messageSource;
+
+    public AccountController(AccountService accountService, MessageSource messageSource) {
         this.accountService = accountService;
+        this.messageSource = messageSource;
     }
 
     @ModelAttribute
@@ -31,14 +38,19 @@ public class AccountController {
     }
 
     @PostMapping("account_new")
-    public String postAccountNew(@Validated AccountForm form, BindingResult result, Model model) {
+    public String postAccountNew(@Validated AccountForm form, BindingResult result,
+            RedirectAttributes redirectAttributes, Locale locale,
+            UriComponentsBuilder ucb) {
         if (result.hasErrors()) {
             return "account_new";
         }
 
         accountService.registerAccount(form.getUsername(), form.getPassword());
 
-        return "redirect:/";
+        redirectAttributes.addFlashAttribute("message",
+                messageSource.getMessage("success.account.register", null, locale));
+
+        return "redirect:" + ucb.path("/login").build().toUri().toString();
     }
 
 }
--- work/metodo/src/main/java/com/re_kid/metodo/controller/LoginController.java
+++ work/metodo/src/main/java/com/re_kid/metodo/controller/LoginController.java
@@ -1,13 +1,15 @@
 package com.re_kid.metodo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class LoginController {
 
     @GetMapping("login")
-    public String getLogin() {
+    public String getLogin(@ModelAttribute("message") String message, Model model) {
         return "login";
     }
 
